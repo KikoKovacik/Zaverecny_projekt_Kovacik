1329b46c4c26ba7f3581b14f41fcb8bb12697346
Krištof Kováčik <kiko.kovacik1502@gmail.com>
2026-04-03 21:03:23 +0200

    Vytvorenie sablony footer.php

--- galeria.php
+++ galeria.php
@@ -52,56 +52,5 @@
             </div>
         </div>
 
-<!--FOOTER VŠADE ROVNAKÝ-->
-        <footer>
-            <div class="footer_vsetko">
-                <div class="footer_hore">
-                    <p>Hlavný sponzor</p>
-                    <div class="footer_preklik">
-                        <a href="https://www.stravovanieulisku.sk/">Stravovanie u Líšku</a>
-                    </div>
-                </div>
-                <div class="footer_stred">
-                    <div class="footer_vlavo">
-                        <img src="img/Zbehy_znak.png" alt="Znak TJ Slovan Zbehy">
-                        <p>TJ Slovan Zbehy</p>
-                    </div>
-                    <div class="footer_vpravo">
-                        <a href="index.php" class="footer_ahref">Domov</a>
-                        <a href="blog.php" class="footer_ahref">Blog</a>
-                        <a href="listky.php" class="footer_ahref">Lístky</a>
-                        <a href="galeria.php" class="footer_ahref">Galéria</a>
-                        <a href="kontakt.php" class="footer_ahref">Kontakt</a>
-                    </div>
-                </div>
-                <div class="footer_dole">
-                    <p>© oficiálna stránka futbalového klubu TJ Slovan Zbehy</p>
-                    <p>Všetky práva vyhradené.</p>
-                    <p>2012 – 2025</p>
-                    <div class="footer_cislo_mail">
-                        <a href="">0912 345 678</a>
-                        <a href="">tjzbehy@sfz.com</a>
-                    </div>
-                </div>
-            </div>
-        </footer>
-            
-        <!--COOKIES A SCRIPTY VŠADE ROVNAKÉ-->
-        <!--KREATÍVNY BOD-->
-        <div id="cookieConsent">
-            Táto stránka používa cookies.
-            <button id="acceptCookies">Rozumiem</button>
-        </div>
-
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-            crossorigin="anonymous"
-        ></script>      
-
-        <script 
-            src="js/script.js">
-        </script>
-    </body>
-</html>
+<?php require_once __DIR__ . '/templates/footer.php'; ?>
 
--- thankyou.php
+++ thankyou.php
@@ -1,20 +1,12 @@
 ﻿<?php require_once __DIR__ . '/templates/header.php'; ?>
 
 <!--TELO THANK YOU-->
-    <body>
-        <!--NADPIS, OBRÁZOK A PREKLIK-->
-        <div class="thankyou_telo">
-            <h1>Ďakujeme Vám za správu. Čoskoro vás budeme kontaktovať.</h1>
-            <img src="img/dakujeme_znak.png" alt="Obrázok vďaky">
-            <div class="thankyou_ahref"> 
-                <a href="index.php">Vrátiť sa na domovskú stránku</a>
-            </div>
-        </div>
-        <!--SCRIPT-->
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-            crossorigin="anonymous"
-        ></script>
-    </body>
-</html>
+<div class="thankyou_telo">
+    <h1>Ďakujeme Vám za správu. Čoskoro vás budeme kontaktovať.</h1>
+    <img src="img/dakujeme_znak.png" alt="Obrázok vďaky">
+    <div class="thankyou_ahref">
+        <a href="index.php">Vrátiť sa na domovskú stránku</a>
+    </div>
+</div>
+
+<?php require_once __DIR__ . '/templates/footer.php'; ?>
