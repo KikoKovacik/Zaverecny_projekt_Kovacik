003d2f3d5fb4df399bd37311f00c2e867e01decd
Krištof Kováčik <kiko.kovacik1502@gmail.com>
2026-04-03 21:04:03 +0200

    Uprava kontaktneho formulara na POST

--- kontakt.php
+++ kontakt.php
@@ -7,18 +7,18 @@
             </div>
             <!--LABELY, INPUTY A TLAČIDLO VO FORMULÁRI-->
             <div class="kontakt_formular">
-                <form class="formular" id="kontaktForm">
+                <form class="formular" id="kontaktForm" method="post" action="kontakt.php">
                     <label for="meno">Meno</label>
-                    <input type="text" id="meno" required>
+                    <input type="text" id="meno" name="meno" required>
 
                     <label for="email">Email</label>
-                    <input type="email" id="email" required>
+                    <input type="email" id="email" name="email" required>
                     
                     <label for="sprava">Správa</label>
-                    <textarea id="sprava" rows="5" placeholder="Svoju správu môžete zanechať ťu" required></textarea>
+                    <textarea id="sprava" name="sprava" rows="5" placeholder="Svoju správu môžete zanechať tu" required></textarea>
 
                     <div class="suhlas">
-                        <input type="checkbox" id="suhlas" required>
+                        <input type="checkbox" id="suhlas" name="suhlas" value="1" required>
                         <label for="suhlas">Súhlasím so spracovaním osobných údajov</label>
                     </div>
 
